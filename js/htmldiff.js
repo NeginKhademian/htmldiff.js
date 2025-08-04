407ecfcb001c8b9ffa8150f5119501c6943e34e8
unknown <neginkhademian@gmail.com>
2025-08-04 08:44:02 +0330

    fix: check by word

--- js/htmldiff.js
+++ js/htmldiff.js
@@ -171,105 +171,64 @@
      * @return {Array.<string>} The list of tokens.
      */
     function htmlToTokens(html){
-        var mode = 'char';
-        var currentWord = '';
-        var currentAtomicTag = '';
-        var words = [];
-        for (var i = 0; i < html.length; i++){
-            var char = html[i];
-            switch (mode){
-                case 'tag':
-                    var atomicTag = isStartOfAtomicTag(currentWord);
-                    if (atomicTag){
-                        mode = 'atomic_tag';
-                        currentAtomicTag = atomicTag;
-                        currentWord += char;
-                    } else if (isStartofHTMLComment(currentWord)){
+    var mode = 'char';
+    var currentWord = '';
+    var currentAtomicTag = '';
+    var words = [];
+
+    for (var i = 0; i < html.length; i++) {
+        var char = html[i];
+
+        switch (mode) {
+            case 'tag':
+                currentWord += char;
+                if (isEndOfTag(char)) {
+                    if (isStartofHTMLComment(currentWord)) {
                         mode = 'html_comment';
-                        currentWord += char;
-                    } else if (isEndOfTag(char)){
-                        currentWord += '>';
-                        words.push(createToken(currentWord));
-                        currentWord = '';
-                        if (isWhitespace(char)){
-                            mode = 'whitespace';
-                        } else {
-                            mode = 'char';
-                        }
                     } else {
-                        currentWord += char;
-                    }
-                    break;
-                case 'atomic_tag':
-                    if (isEndOfTag(char) && isEndOfAtomicTag(currentWord, currentAtomicTag)){
-                        currentWord += '>';
                         words.push(createToken(currentWord));
                         currentWord = '';
-                        currentAtomicTag = '';
                         mode = 'char';
-                    } else {
-                        currentWord += char;
                     }
-                    break;
-                case 'html_comment':
+                }
+                break;
+
+            case 'html_comment':
+                currentWord += char;
+                if (isEndOfHTMLComment(currentWord)) {
+                    words.push(createToken(currentWord));
+                    currentWord = '';
+                    mode = 'char';
+                }
+                break;
+
+            case 'char':
+                if (isStartOfTag(char)) {
+                    if (currentWord) {
+                        currentWord.split(/(\s+)/).forEach(function(word) {
+                            if (word) words.push(createToken(word));
+                        });
+                    }
+                    currentWord = '<';
+                    mode = 'tag';
+                } else {
                     currentWord += char;
-                    if (isEndOfHTMLComment(currentWord)){
-                        currentWord = '';
-                        mode = 'char';
-                    }
-                    break;
-                case 'char':
-                    if (isStartOfTag(char)){
-                        if (currentWord){
-                            words.push(createToken(currentWord));
-                        }
-                        currentWord = '<';
-                        mode = 'tag';
-                    } else if (/\s/.test(char)){
-                        if (currentWord){
-                            words.push(createToken(currentWord));
-                        }
-                        currentWord = char;
-                        mode = 'whitespace';
-                    } else if (/[\w\d\#@]/.test(char)){
-                        currentWord += char;
-                    } else if (/&/.test(char)){
-                        if (currentWord){
-                            words.push(createToken(currentWord));
-                        }
-                        currentWord = char;
-                    } else {
-                        currentWord += char;
-                        words.push(createToken(currentWord));
-                        currentWord = '';
-                    }
-                    break;
-                case 'whitespace':
-                    if (isStartOfTag(char)){
-                        if (currentWord){
-                            words.push(createToken(currentWord));
-                        }
-                        currentWord = '<';
-                        mode = 'tag';
-                    } else if (isWhitespace(char)){
-                        currentWord += char;
-                    } else {
-                        if (currentWord){
-                            words.push(createToken(currentWord));
-                        }
-                        currentWord = char;
-                        mode = 'char';
-                    }
-                    break;
-                default:
-                    throw new Error('Unknown mode ' + mode);
-            }
+                }
+                break;
+
+            default:
+                throw new Error('Unknown mode: ' + mode);
         }
-        if (currentWord){
-            words.push(createToken(currentWord));
-        }
-        return words;
     }
+
+    if (currentWord) {
+        currentWord.split(/(\s+)/).forEach(function(word) {
+            if (word) words.push(createToken(word));
+        });
+    }
+
+    return words;
+}
 
     /**
      * Creates a key that should be used to match tokens. This is useful, for example, if we want
